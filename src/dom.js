8c325d467e6ddb8854bfe73806b20b5e5b44267c
Joey Patterson <joeypatterson177@gmail.com>
2022-12-01 02:35:18 -0500

    Add following features:
     - create blank form.js file
     - create dom.js file to add projects and tasks to projects to page
     - update css for new DOM features

---
+++ src/dom.js
@@ -0,0 +1,40 @@
+export default function addToDOM(project) {
+    const container = document.querySelector('.body');
+    const item = document.createElement('div');
+    item.classList.add('project');
+    container.appendChild(item);
+
+    const projectHeader = document.createElement('div');
+    projectHeader.classList.add('projectHeader');
+    item.appendChild(projectHeader);
+
+    const title = document.createElement('h3');
+    title.innerHTML = `<u>${project.title}<u>`;
+    title.classList.add('projectTitle');
+    projectHeader.appendChild(title);
+
+    //need to add task to event listener
+    const add = document.createElement('h1');
+    add.addEventListener('click', () => {
+        addTaskToProject(event);
+    });
+    add.innerHTML = '+';
+    add.classList.add('taskAdd');
+    projectHeader.appendChild(add);
+};
+
+function addTaskToProject(event, task) {
+    const project = event.target.parentElement.parentElement;
+
+    const projectTasks = document.createElement('div');
+    projectTasks.classList.add('projectTasks');
+    project.appendChild(projectTasks);
+
+    const testText = document.createElement('h3');
+    testText.classList.add('taskTitle');
+    testText.innerHTML = 'Test';
+    projectTasks.appendChild(testText)
+    console.log(project);
+};
+
+export { addToDOM, addTaskToProject}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import { createProject, createTask } from "./tasks";
+import { addToDOM, addTaskToProject} from "./dom";
 
-let project1 = new createProject('go to work')
+let projectArray = [];
+
+
+//create main control logic function
+const btn = document.querySelector('.add');
+btn.addEventListener('click', () => {
+    let project2 = new createProject('Get ready for bed');
+    addToDOM(project2);
+});
+
+let project0 = new createProject('Go to work');
+let project1 = new createProject('Coach basketball');
+addToDOM(project0);
+addToDOM(project1);
 let task1 = new createTask('sweep', 'clean the floors', 'Dec 2nd', 'high');
-console.log(project1, task1);
+console.log(projectArray);
